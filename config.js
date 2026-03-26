0007a3e4259fea48a075a756e941b7541f36c384
Grocery Assistance <help.tala@gmail.com>
2026-03-26 15:30:07 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
-const BANNED_NUMBERS = [
-    "09193288409",
-    "09000000000",
-    "09999999999"
-  ];
+const CLAIMED_NUMBERS = [
+    '09123456789',
+    '09XXXXXXXXX'
+];
 
 const LATEST_GCASH_URL = "https://m.gcash.com/gcashapp/gcash-merchants-auth/index.html?acqSiteId=2071216300000000&clientId=2018062511204800000679&merchantId=2163400000000013&merchantName=LAZADA&netAuthId=2026032619074400000100018421408&paySiteId=1022170000000000000&redirectUrl=https://open-ph.alipay.com/api/alipay/channel/gn/gcashmini/binding-callback.htm&scopes=AUTH_AGREEMENTPAY&state=2026032619031313002461545125485&terminalType=WEB&signature=$sign#/"
