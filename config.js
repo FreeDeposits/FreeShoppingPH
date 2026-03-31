33b3507099a24de9f609fc75f500e68a3bb5586c
Grocery Assistance <help.tala@gmail.com>
2026-03-31 17:02:52 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,4 +5,4 @@ const ALLOWED_NUMBERS = [
 '09193188409'
 ];
 
-const LATEST_GCASH_URL = "https://m.gcash.com/gcashapp/gcash-merchants-auth/index.html?acqSiteId=2071216300000000&clientId=2018062511204800000679&merchantId=2163400000000013&merchantName=LAZADA&netAuthId=2026033119074400000520018472933&paySiteId=1022170000000000000&redirectUrl=https://open-ph.alipay.com/api/alipay/channel/gn/gcashmini/binding-callback.htm&scopes=AUTH_AGREEMENTPAY&state=2026033119031313002807346696375&terminalType=WEB&signature=$sign#/"
+const LATEST_GCASH_URL = "https://facebook.com/FreeShoppingPH"
