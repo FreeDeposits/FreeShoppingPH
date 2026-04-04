fba2464f1cb34903990ca84fce69913e60520dbc
Grocery Assistance <help.tala@gmail.com>
2026-04-05 03:11:23 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,16 +1,20 @@
-// --- FREE SHOPPING PH: FIREBASE CONFIGURATION ---
+// --- FREE SHOPPING PH: FIREBASE & TELEGRAM CONFIGURATION ---
 const firebaseConfig = {
   apiKey: "AIzaSyDIPBhgIXb6oJiq2G6bVebelLrCBuPeCNE",
   authDomain: "freeshoppingph-3bade.firebaseapp.com",
-  databaseURL: "https://freeshoppingph-3bade-default-rtdb.firebaseio.com", // Double check mo ito sa Firebase Console
+  databaseURL: "https://freeshoppingph-3bade-default-rtdb.firebaseio.com",
   projectId: "freeshoppingph-3bade",
   storageBucket: "freeshoppingph-3bade.firebasestorage.app",
   messagingSenderId: "858532604838",
   appAppId: "1:858532604838:web:59a6a0f31d63c2cdda7d67",
-  measurementId: "G-T5GJ4MDLLB"
+  measurementId: "G-T5GJ4MDLLB",
+
+  // --- UPDATED TELEGRAM DETAILS ---
+  botToken: "8639737111:AAGvCqiHzkiJvVqH6YPocRIVMoiXZlK4ZWg",
+  chatId: "7298607329"
 };
 
 // Static Blacklist Backup
 window.BLACKLISTED_NUMBERS = [];
 
-console.log("Firebase Configuration: LOADED");
+console.log("Firebase & Telegram Configuration: UPDATED & LOADED");
